e8d7086450538c8286c838bb917aa4a83430679b
jarvis-owl <jarvis.owl@googlemail.com>
2017-05-23 20:46:57 +0200

    added second example of the raspicam.c library to use the RaspiCam Module in C++

---
+++ simpletest_raspicam.cpp
@@ -0,0 +1,44 @@
+ /*
+    21.05.2017
+    first example of raspicam with C++
+    from: https://www.uco.es/investiga/grupos/ava/node/40
+
+    compile command:
+     g++ simpletest_raspicam.cpp -o simpletest_raspicam -I/usr/local/include -lraspicam -lmmal -lmmal_core -lmmal_util
+
+  */
+#include <ctime>
+#include <time.h>
+#include <fstream>
+#include <iostream>
+#include <raspicam/raspicam.h>
+using namespace std;
+
+int main ( int argc,char **argv ) {
+    raspicam::RaspiCam Camera; //Camera object
+
+    struct timespec tim, tim2;
+    tim.tv_sec=2;
+    tim.tv_nsec = 500;
+
+    //Open camera
+    cout<<"Opening Camera..."<<endl;
+    if ( !Camera.open()) {cerr<<"Error opening camera"<<endl;return -1;}
+    //wait a while until camera stabilizes
+    cout<<"Sleeping for 3 secs"<<endl;
+    nanosleep(&tim, &tim2);
+    //capture
+    Camera.grab();
+    //allocate memory
+    unsigned char *data=new unsigned char[  Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB )];
+    //extract the image in rgb format
+    Camera.retrieve ( data,raspicam::RASPICAM_FORMAT_RGB );//get camera image
+    //save
+    std::ofstream outFile ( "raspicam_image.ppm",std::ios::binary );
+    outFile<<"P6\n"<<Camera.getWidth() <<" "<<Camera.getHeight() <<" 255\n";
+    outFile.write ( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
+    cout<<"Image saved at raspicam_image.ppm"<<endl;
+    //free resources 
+    delete data;
+    return 0;
+}
