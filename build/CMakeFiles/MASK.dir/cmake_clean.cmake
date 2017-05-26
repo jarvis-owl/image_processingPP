044b173c3b28c298b12122905cd6b7315acc3e2f
jarvis-owl <jarvis.owl@googlemail.com>
2017-05-26 22:06:41 +0200

    compile MASK.cpp successfully

---
+++ build/CMakeFiles/MASK.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MASK.dir/MASK.cpp.o"
+  "MASK.pdb"
+  "MASK"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MASK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -46,5 +46,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/simpleCV.dir/DependInfo.cmake"
+  "CMakeFiles/MASK.dir/DependInfo.cmake"
   )
