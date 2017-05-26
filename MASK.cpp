cfaca76378ac8cea63b6bda000a10fab55027f7c
jarvis-owl <jarvis.owl@googlemail.com>
2017-05-26 21:48:06 +0200

    initiated MASK.cpp

--- MASK.cpp
+++ MASK.cpp
@@ -1,48 +1,49 @@
 
 /*
   26.05.'17
-  this is an original example for OPENCV from the raspicam creators
-
-  source:
-  https://www.uco.es/investiga/grupos/ava/node/40
-  https://sourceforge.net/projects/raspicam/files/?
-
+  this should server as start/mask file for future code.
+  derived from old/simpleCV.cpp [standard example from resource]
 */
 
-#include <ctime>
+
 #include <iostream>
 #include <raspicam/raspicam_cv.h>
 using namespace std;
 
 int main ( int argc,char **argv ) {
 
-    time_t timer_begin,timer_end;
+
     raspicam::RaspiCam_Cv Camera;
     cv::Mat image;
     int nCount=100;
+
     //set camera params
     Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
+
     //Open camera
     cout<<"Opening Camera..."<<endl;
     if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
+
     //Start capture
     cout<<"Capturing "<<nCount<<" frames ...."<<endl;
-    time ( &timer_begin );
+
     for ( int i=0; i<nCount; i++ ) {
         Camera.grab();
         Camera.retrieve ( image);
-        if ( i%5==0 )  cout<<"\r captured "<<i<<" images"<<std::flush<<endl;
+        if ( i%5==0 )  cout<<"\r captured "<<i<<" images"<<std::flush;
     }
+    cout << endl;
     cout<<"Stop camera..."<<endl;
     Camera.release();
-    //show time statistics
-    time ( &timer_end ); /* get current time; same as: timer = time(NULL)  */
-    double secondsElapsed = difftime ( timer_end,timer_begin );
-    cout<< secondsElapsed<<" seconds for "<< nCount<<"  frames : FPS = "<<  ( float ) ( ( float ) ( nCount ) /secondsElapsed ) <<endl;
+
+
     //save image
     cv::imwrite("raspicam_cv_image.jpg",image);
     cout<<"Image saved at raspicam_cv_image.jpg"<<endl;
+
+    return 0;
 }
+
 /*
 ============================ COMPILING ============================
 Now, create a file named CMakeLists.txt and add:
@@ -53,8 +54,8 @@ find_package(raspicam REQUIRED)
 find_package(OpenCV)
 IF  ( OpenCV_FOUND AND raspicam_CV_FOUND)
 	MESSAGE(STATUS "COMPILING OPENCV TESTS")
-	add_executable (simpletest_raspicam_cv simpletest_raspicam_cv.cpp)
-	target_link_libraries (simpletest_raspicam_cv ${raspicam_CV_LIBS})
+	add_executable (MASK MASK.cpp)
+	target_link_libraries (MASK ${raspicam_CV_LIBS})
 ELSE()
 	MESSAGE(FATAL_ERROR "OPENCV NOT FOUND IN YOUR SYSTEM")
 ENDIF()
@@ -65,6 +66,6 @@ mkdir build
 cd build
 cmake ..
 make
-./simpletest_raspicam_cv
+./MASK
 
 */
