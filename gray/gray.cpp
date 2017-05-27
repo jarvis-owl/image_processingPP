18d47c00b4da4f1e9722a539dfea9d1d0143bbd9
jarvis-owl <jarvis.owl@googlemail.com>
2017-05-27 22:08:45 +0200

    okay he noticed , i added nothing :P - i really have to think about syncing RPi and Mint ftp/github

--- gray/gray.cpp
+++ gray/gray.cpp
@@ -1,80 +1,35 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
+#include <opencv/cv.h>
+#include "opencv2/highgui/highgui.hpp" //#include <highgui.h>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
 
-using namespace std;
 using namespace cv;
 
-/**
- * @function main
- */
 int main( int argc, char** argv )
 {
-  Mat src, dst;
+ char* imageName = argv[1];
 
-  /// Load image
-  src = imread( argv[1], 1 );
+ Mat image;
+ image = imread( imageName, 1 );
 
-  if( !src.data )
-    { printf("reading image not successfull");return -1; }
+ if( argc != 2 || !image.data )
+ {
+   printf( " No image data \n " );
+   return -1;
+ }
 
-  /// Separate the image in 3 places ( B, G and R )
-  vector<Mat> bgr_planes;
-  split( src, bgr_planes );
+//CONVERT
+ Mat gray_image;
+ cvtColor( image, gray_image, CV_BGR2GRAY );
 
-  /// Establish the number of bins
-  int histSize = 256;
+ imwrite( "Gray_Image.jpg", gray_image );
+/*
+ namedWindow( imageName, CV_WINDOW_AUTOSIZE );
+ namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
+ imshow( imageName, image );
+ imshow( "Gray image", gray_image );
+ waitKey(0);
+*/
 
-  /// Set the ranges ( for B,G,R) )
-  float range[] = { 0, 256 } ;
-  const float* histRange = { range };
-
-  bool uniform = true; bool accumulate = false;
-
-  Mat b_hist, g_hist, r_hist;
-
-  /// Compute the histograms:
-  calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-  calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-  calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
-
-  // Draw the histograms for B, G and R
-  int hist_w = 512; int hist_h = 400;
-  int bin_w = cvRound( (double) hist_w/histSize );
-
-  Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-
-  /// Normalize the result to [ 0, histImage.rows ]
-  normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-  normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-  normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-  /// Draw for each channel
-  for( int i = 1; i < histSize; i++ )
-  {
-      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-                       Scalar( 255, 0, 0), 2, 8, 0  );
-      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-                       Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-                       Scalar( 0, 255, 0), 2, 8, 0  );
-      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-                       Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-                       Scalar( 0, 0, 255), 2, 8, 0  );
-  }
-
-  /// Display -- commented, due to missing display
-  //namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
-  //imshow("calcHist Demo", histImage );
-  if(imwrite("histogram.jpg",histImage) )
-  {
-    printf("imwrite returned true");
-  }
-
-
-
-  waitKey(0);
-
-  return 0;
+ return 0;
 }
